26894ab61072b39925e166eeaf137d2f09e4c0f1
strontium-rn <pathakaaryan23@gmail.com>
2024-07-06 11:23:03 +0545

    constructor

--- concepts/constructors.cpp
+++ concepts/constructors.cpp
@@ -1,11 +1,29 @@
-#include<iostream>>
+#include<iostream>
 using namespace std;
+
+const double PI=3.14;
 
 class circle{
     private: 
-        int raidus; 
+        int radius; 
     public:
-        circle(int r){
-            
+        circle(int r): radius(r){
+        } 
+        double computeArea(){
+            return PI*radius*radius;
+        }       
+        void display(){
+            cout<<"The area of circle is: "<<computeArea()<<endl;
         }
+        
+
 }; 
+int main(){
+    int r;
+    cout<<"Enter the radius:"<<endl;
+    cin>>r;
+
+    circle obj(r);
+    obj.display();
+    return 0;
+}
