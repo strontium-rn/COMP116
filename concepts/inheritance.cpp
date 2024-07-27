43ecce5d30ea54b869cf8cf9b4a873fb06627321
strontium-rn <pathakaaryan23@gmail.com>
2024-07-27 23:10:38 +0545

    errors

--- concepts/inheritance.cpp
+++ concepts/inheritance.cpp
@@ -10,11 +10,19 @@ using namespace std;
 class Point{
     public:
     int x, y;
-    Point(int x=0, y=0): x(x), y(y) {}
+    Point(int x=0, int y=0): x(x), y(y) {}
 };
 
 class Polygon{
     public:
     int numberOfSides;
     Point centroid;    
+    Polygon(int side, Point center): numberOfSides(side), centroid(center){}
+
+    void display(){
+        cout<<"Number of sides and centroid coordinates is:"<<numberOfSides centroid <<endl;
+    }
+    void move(int newX, int newY){
+        cout<<"New x and y coordinates are:"<<newX, newY<<endl;
+    }
 };
