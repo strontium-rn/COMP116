0cf6acfb93eb5f31bdecae7981dfaad4268a1eed
strontium-rn <pathakaaryan23@gmail.com>
2024-08-15 09:29:00 +0545

    operator overloading

---
+++ lab_exam/operatorOverloading.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+    private:
+        int real;
+        int imag;
+    public:
+        Complex(int r=0, int i=0){
+            real =r;
+            imag = i;
+        }
+
+        Complex operator+ (Complex const& obj){
+            Complex res;
+            res.real = real + obj.real;
+            res.imag = imag + obj.imag;
+            return res;
+        }
+
+        void display(){
+            cout<<real << "+i"<< imag;
+        }
+};
+int main(){
+    Complex c1, c2(5,10), c3(9,12);
+    c1 = c2+c3;
+    c1.display();
+}
