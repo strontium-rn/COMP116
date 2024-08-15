74888b03b0507a68f7cfa36356ad1a8802b85bc1
strontium-rn <pathakaaryan23@gmail.com>
2024-08-15 13:12:08 +0545

    KU

---
+++ KU.cpp
@@ -0,0 +1,94 @@
+// Define a class Person with attributes like name, age, and address. 
+// Derive two classes Student and Professor from Person. 
+// In Student, include attributes like studentID, course, and grades.
+// In Professor, include employeeID, subject, and salary. 
+// Implement functions to calculate the GPA for students and the salary for professors 
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Person{
+    private:
+        string name;
+        int age;
+        string address;
+    public:
+        Person(){}
+        void setInfo(string n, int ag, string ad){
+            name = n;
+            age = ag;
+            address = ad;
+        }
+        virtual double gpa(){
+            return 0;
+        }
+        virtual double calculateSalary(){
+            return 0;
+        }
+        void display(){
+            cout<<"Name:"<<name;
+            cout<<"Age:"<<age;
+            cout<<"Address:"<<address;
+            
+            
+           
+        }
+};
+class Student: public Person{
+    private:
+        int studentID;
+        string course;
+        int percentage;
+    public:
+        void setInfoStu(int si, string cr, int pr){
+            studentID = si;
+            course = cr;
+            percentage = pr;
+        }
+        double gpa()override{
+            return percentage/25.0;
+        }
+        virtual void display(){
+            Person::display();
+            cout<<"GPA of student is:"<<gpa()<<endl;
+        }
+};
+
+class Professor: public Person{
+    private:
+        int employeeId;
+        int salary;
+        string subject;
+        int noOfClass;
+        int perClass;
+    public:
+        void setInfoProf(int ei, string su, int ns, int ps){
+            employeeId = ei;
+            subject = su;
+            noOfClass = ns;
+            perClass = ps;
+        }
+        double calculateSalary()override{
+            return noOfClass * perClass; 
+        }
+        virtual void display(){
+            Person::display();
+            cout<<"Salary of Professor is:"<<calculateSalary()<<endl;
+        }
+};
+
+int main(){
+    Student s;
+    
+    s.setInfo("Aarya", 19, "Kathmandu");
+    s.setInfoStu(23, "CS", 78);
+    s.display();
+
+    Professor p;
+    
+    p.setInfo("Tamrakar",58, "Zoo");
+    p.setInfoProf(4323, "COMP116", 22, 3000);
+    p.display();
+
+    return 0;
+}
---
+++ templatesBubbleSort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+template <typename T> void bubblesort(T a[], int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = n-1; i < j; j--)
+        {
+            if (a[j]< a[j-1])
+                swap(a[j], a[j-1]);       
+                       
+        }        
+    }    
+}
+
+int main(){
+    int a[5] = {44,55,33,22,11};
+    int n = sizeof(a)/sizeof(a[0]);
+
+    bubblesort(a,n);
+    cout<<"Sorted Array: "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<a[i] <<endl;
+    }
+    return 0;        
+}
